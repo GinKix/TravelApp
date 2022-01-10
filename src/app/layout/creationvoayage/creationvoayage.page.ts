3c1ba3aae10a4581c1852890e0939c1d5a573009
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-10 12:06:21 +0100

    Page creation voyage

---
+++ src/app/layout/creationvoayage/creationvoayage.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creationvoayage',
+  templateUrl: './creationvoayage.page.html',
+  styleUrls: ['./creationvoayage.page.scss'],
+})
+export class CreationvoayagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
