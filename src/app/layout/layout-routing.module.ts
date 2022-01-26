eb24d169b3ae36e751bb63aedf357f129df35427
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-26 21:40:25 +0100

    Add files via upload

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,29 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { LayoutPage } from './layout.page';
+import { LayoutPage } from "./layout.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: LayoutPage,
     children: [
       // Route qui charge le module du profil utilisateur
       {
-        path: 'profil-user',
-        loadChildren: () => 
-          import('./profil-user/profil-user.module').then( 
+        path: "profil-user",
+        loadChildren: () =>
+          import("./profil-user/profil-user.module").then(
             (m) => m.ProfilUserPageModule
           ),
       },
 
       // Route qui charge le module de la liste des voyages
       {
-        path: 'liste-voyages',
-        loadChildren: () => 
-          import('./liste-voyages/liste-voyages.module').then( 
+        path: "liste-voyages",
+        loadChildren: () =>
+          import("./liste-voyages/liste-voyages.module").then(
             (m) => m.ListeVoyagesPageModule
           ),
+      },
+      {
+        path: "create-trip",
+        loadChildren: () =>
+          import("./create-trip/create-trip.module").then(
+            (m) => m.CreateTripPageModule
+          ),
+      },
+      {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "liste-voyages",
       },
     ],
   },
