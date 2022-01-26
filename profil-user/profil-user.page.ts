e87d1ae244c6e29a6c2bd6faa8ebc601c7164012
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-26 21:19:07 +0100

    Update App - profil user

---
+++ profil-user/profil-user.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
+import { IRegister } from "src/app/models/iregister";
+
+@Component({
+  selector: "app-profil-user",
+  templateUrl: "./profil-user.page.html",
+  styleUrls: ["./profil-user.page.scss"],
+})
+export class ProfilUserPage implements OnInit {
+  updateUserPayload: IRegister;
+  updateUserError: boolean;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.updateUserPayload = {
+      name: undefined,
+      password: undefined,
+    };
+  }
+
+  ngOnInit(): void {}
+
+  /**
+   * Called when the login form is submitted.
+   */
+  onSubmit(form: NgForm) {
+    // Do not do anything if the form is invalid.
+    if (form.invalid) {
+      return;
+    }
+
+    // Hide any previous login error.
+    this.updateUserError = false;
+
+    // Perform the authentication request to the API.
+    /*this.auth.register$(this.updateUserPayload).subscribe({
+      next: () => this.router.navigateByUrl('/login'),
+      error: (err) => {
+        this.updateUserError = true;
+        console.warn(`Registration failed: ${err.message}`);
+      },
+    });*/
+  }
+
+  // Add a method to log out.
+  logOut() {
+    console.log("logging out...");
+    this.auth.logOut();
+    this.router.navigateByUrl("/login");
+  }
+}
