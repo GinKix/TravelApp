289b417507b0c001b86af037a23534c98c40afd4
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-26 21:14:57 +0100

    Update App

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+// Copy this file to environment.ts and replace the values with your configuration
+export const environment = {
+    production: false,
+    apiUrl: "<your-api-url>",
+    // TODO: add the qimg URL and secret token to the configuration
+    qimgUrl: "<your-api-url>",
+    qimgSecret: "<change-with-your-token>",
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Copy this file to environment.ts and replace the values with your configuration
+export const environment = {
+    production: false,
+    apiUrl: "https://comem-travel-log-api.herokuapp.com/api",
+    // TODO: add the qimg URL and secret token to the configuration
+    qimgUrl: "https://comem-qimg.herokuapp.com/api",
+    qimgSecret: "ABCDEF123456789",
+  };
