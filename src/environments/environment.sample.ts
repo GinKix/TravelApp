a71709c05a28da44efa00d919aaa07cb3250dc40
Kilian Crettaz <kilian.crettaz@heig-vd.ch>
2022-01-28 11:20:59 +0100

    Ajout du fichier environment.sample

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: "<your-api-url>",
+    // TODO: add the qimg URL and secret token to the configuration
+    qimgUrl: "<your-api-url>",
+    qimgSecret: "<change-with-your-token>",
+  };
