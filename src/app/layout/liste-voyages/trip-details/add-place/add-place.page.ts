8f07d43cb0c06c3721cbb301b4b8cde08a09ca08
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-29 12:17:09 +0100

    Update add place

--- src/app/layout/liste-voyages/trip-details/add-place/add-place.page.ts
+++ src/app/layout/liste-voyages/trip-details/add-place/add-place.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PlaceService } from '../place.service';
 
 @Component({
   selector: 'app-add-place',
@@ -9,14 +10,50 @@ import { Router } from '@angular/router';
 })
 export class AddPlacePage implements OnInit {
   createPlaceForm: FormGroup;
+  tripID: string;
+  tripHref: string;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private placeService: PlaceService
+  ) {
     this.createPlaceForm = this.formBuilder.group({
       title: [null, [Validators.required]],
       description: [null, [Validators.required]],
+      latitude: [null, [Validators.required]],
+      longitude: [null, [Validators.required]],
       pictureUrl: [null, null],
     });
+
+    this.tripID = this.activatedRoute.snapshot.params.tripID;
+    this.tripHref = this.activatedRoute.snapshot.params.tripHref;
   }
 
   ngOnInit(): void {}
+
+  onCreatePlace() {
+    this.placeService
+      .createPlace({
+        description: this.createPlaceForm.controls.description.value,
+        name: this.createPlaceForm.controls.title.value,
+        tripHref: this.tripHref,
+        location: {
+          type: 'Point',
+          coordinates: [
+            this.createPlaceForm.controls.latitude.value,
+            this.createPlaceForm.controls.longitude.value,
+          ],
+        },
+        tripID: this.tripID,
+      })
+      .subscribe(() => {
+        this.router.navigate([
+          '/liste-voyages/trip-details/',
+          this.tripID,
+          this.tripHref,
+        ]);
+      });
+  }
 }
