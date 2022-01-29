948c947d3a36aed6dede010a9529de37dccb6473
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2022-01-29 15:23:33 +0100

    Update travel App

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,43 +1,57 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { LayoutPage } from "./layout.page";
+import { LayoutPage } from './layout.page';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: LayoutPage,
     children: [
       // Route qui charge le module du profil utilisateur
       {
-        path: "profil-user",
+        path: 'profil-user',
         loadChildren: () =>
-          import("./profil-user/profil-user.module").then(
+          import('./profil-user/profil-user.module').then(
             (m) => m.ProfilUserPageModule
           ),
       },
 
       // Route qui charge le module de la liste des voyages
       {
-        path: "liste-voyages",
+        path: 'liste-voyages',
         loadChildren: () =>
-          import("./liste-voyages/liste-voyages.module").then(
+          import('./liste-voyages/liste-voyages.module').then(
             (m) => m.ListeVoyagesPageModule
           ),
       },
       {
-        path: "create-trip",
+        path: 'create-trip',
         loadChildren: () =>
-          import("./create-trip/create-trip.module").then(
+          import('./create-trip/create-trip.module').then(
             (m) => m.CreateTripPageModule
           ),
       },
       {
-        path: "",
-        pathMatch: "full",
-        redirectTo: "liste-voyages",
+        path: 'update-trip',
+        loadChildren: () =>
+          import('./update-trip/update-trip.module').then(
+            (m) => m.UpdateTripPageModule
+          ),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'liste-voyages',
       },
     ],
+  },
+  {
+    path: 'update-trip',
+    loadChildren: () =>
+      import('./update-trip/update-trip.module').then(
+        (m) => m.UpdateTripPageModule
+      ),
   },
 ];
 
--- src/app/layout/layout.page.ts
+++ src/app/layout/layout.page.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 //Import pour l'authentification et inclure le l ogout
 import { Router } from '@angular/router';
-import { AuthService } from "src/app/auth/auth.service";
+import { AuthService } from 'src/app/auth/auth.service';
 
 // Type custom représentant les données d'une page
 declare type PageTab = {
   title: string; // Le titre de la page dans la barre de navigation
   icon: string; // L'icône de la page dans la barre de navigation
   path: string; // Le chemin de la page à afficher
-}
+};
 
 @Component({
   selector: 'app-layout',
@@ -24,18 +24,20 @@ export class LayoutPage {
     private auth: AuthService,
     // Inject the router
     private router: Router
-  ) { 
+  ) {
     this.tabs = [
-      {title: "Mon Compte", icon: "person-circle-outline", path:"profil-user"},
-      {title: "Liste des Voyages", icon: "list", path:"liste-voyages"},
-    ]
+      {
+        title: 'Mon Compte',
+        icon: 'person-circle-outline',
+        path: 'profil-user',
+      },
+      { title: 'Liste des Voyages', icon: 'list', path: 'liste-voyages' },
+    ];
   }
 
-  ngOnInit(){}
-
-  logOut(){
-    console.log("logging out...");
+  logOut() {
+    console.log('logging out...');
     this.auth.logOut();
-    this.router.navigateByUrl("/login");
+    this.router.navigateByUrl('/login');
   }
 }
